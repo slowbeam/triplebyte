ec4129066e41162409215d0c51f97d634534d2b3
Sandy <sedwardscode@gmail.com>
2018-11-05 12:47:17 -0500

    ready for building

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import TestComponent from "./components/TestComponent";
-import TestComponentTwo from "./components/TestComponentTwo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TestComponentTwo />
+        <TestComponent />
       </div>
     );
   }
